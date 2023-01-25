ba44cf19d7e0ee69e8172f6fb97e1f350d1dff7e
Guilherme <gui.mochila@gmail.com>
2023-01-25 11:10:31 +0100

    remap: Vim-fugivite blame

--- lua/guimochila/remap.lua
+++ lua/guimochila/remap.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+
+-- Fugitive remap
+vim.keymap.set("n", "<leader>gb", ":silent G blame<CR>", { silent = true })
