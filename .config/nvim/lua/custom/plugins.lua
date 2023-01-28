27f11dd650f5b84e00817edbee4f1d6e0fb8c8a6
Guilherme <gui.mochila@gmail.com>
2023-01-28 16:19:23 +0100

    Add neo-tree

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -9,6 +9,16 @@ return function(use)
   use('thePrimeagen/harpoon')
   use('christoomey/vim-tmux-navigator')
 
+  use({
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  })
+
   -- Colorschemes
 
   -- LSP
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -129,10 +129,20 @@ _G.packer_plugins = {
     path = "/Users/scaldela/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
+  ["neo-tree.nvim"] = {
+    loaded = true,
+    path = "/Users/scaldela/.local/share/nvim/site/pack/packer/start/neo-tree.nvim",
+    url = "https://github.com/nvim-neo-tree/neo-tree.nvim"
+  },
   ["neodev.nvim"] = {
     loaded = true,
     path = "/Users/scaldela/.local/share/nvim/site/pack/packer/start/neodev.nvim",
     url = "https://github.com/folke/neodev.nvim"
+  },
+  ["nui.nvim"] = {
+    loaded = true,
+    path = "/Users/scaldela/.local/share/nvim/site/pack/packer/start/nui.nvim",
+    url = "https://github.com/MunifTanjim/nui.nvim"
   },
   ["null-ls.nvim"] = {
     loaded = true,
@@ -166,6 +176,11 @@ _G.packer_plugins = {
     needs_bufread = false,
     path = "/Users/scaldela/.local/share/nvim/site/pack/packer/opt/nvim-treesitter-textobjects",
     url = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects"
+  },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/Users/scaldela/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["onedark.nvim"] = {
     loaded = true,
