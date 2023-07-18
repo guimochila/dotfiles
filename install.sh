119f7cde6601dda4472d8d8e63ae38a63c73fbcd
Guilherme <gui.mochila@gmail.com>
2023-07-18 11:53:27 +0200

    feature: Using ansible to setup personal configuration

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+
+if ! type "brew" &> /dev/null; then
+  echo "Brew not installed! Installing..."
+  source <"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Brew installed...skipping brew installation"
+fi
+
+if ! type "ansible-playbook" &> /dev/null; then
+  echo "Ansible not installed! Installing..."
+  brew update && brew install ansible
+else 
+  echo "Ansible installed. Done!"
+fi
+
+if [ -f "./personal.yml" ]; then
+  ansible-playbook ./personal.yml
+else
+  echo "No playbook found."
+fi
