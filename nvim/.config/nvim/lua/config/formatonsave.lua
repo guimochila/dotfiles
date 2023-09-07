7d8c8091380e863eaf8398e7decbb7f5e1cb7d56
Guilherme <gui.mochila@gmail.com>
2023-09-07 09:22:24 +0200

    fix: BufWritePre

--- nvim/.config/nvim/lua/config/formatonsave.lua
+++ nvim/.config/nvim/lua/config/formatonsave.lua
@@ -1,6 +1,6 @@
 local group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
 
-vim.api.nvim_create_autocmd("BufWritePost",
+vim.api.nvim_create_autocmd("BufWritePre",
   {
     pattern = { "*.html", "*.jsx", "*.js", "*.tsx", "*.ts" },
     desc = "Auto-format web dev files with Prettier",
