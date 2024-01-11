2a243bc30d37232cf68253cca452fb557f9460da
Guilherme <gui.mochila@gmail.com>
2024-01-11 10:52:13 +0100

    fix: Signcolumn width

--- nvim/.config/nvim/lua/guimochila/core/settings.lua
+++ nvim/.config/nvim/lua/guimochila/core/settings.lua
@@ -20,6 +20,8 @@ vim.opt.colorcolumn = "80"
 vim.opt.textwidth = 80
 vim.opt.termguicolors = true
 
+vim.wo.signcolumn = "yes"
+
 -- Format on save via LSP
 vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
 
