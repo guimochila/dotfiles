bf234b41171d58c88bd19dfbd20284d9be7614e3
Guilherme <gui.mochila@gmail.com>
2024-01-13 11:11:09 +0100

    feat(nvim): Fix colors

--- nvim/.config/nvim/lua/guimochila/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/guimochila/plugins/colorscheme.lua
@@ -1,3 +1,9 @@
+local function apply_colors()
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = 0, bg = "#092135" })
+  vim.api.nvim_set_hl(0, "VisualActive", { bg = "#1d3b88" })
+end
 -- Color scheme
 return {
   -- {
@@ -9,11 +15,7 @@ return {
   --   lazy = true, -- make sure we load this during startup if it is your main colorscheme
   --   -- priority = 1000, -- make sure to load this before all the other start plugins
   --   config = function()
-  --     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  --     vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = 0, bg = "#092135" })
-  --     vim.api.nvim_set_hl(0, "MatchParen", { bg = "#00a884" })
-  --     vim.api.nvim_set_hl(0, "VisualActive", { bg = "#1d3b88" })
+  --    apply_colors()
   --   end,
   -- },
   {
@@ -22,6 +24,7 @@ return {
     priority = 1000,
     config = function()
       vim.cmd.colorscheme('kanagawa-wave')
+      apply_colors()
     end
   },
 }
