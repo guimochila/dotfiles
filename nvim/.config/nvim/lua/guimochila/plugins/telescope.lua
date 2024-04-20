ea1463f78f30f42fa6356acae01425daf730e9c3
Guilherme <gui.mochila@gmail.com>
2024-04-20 15:29:31 +0200

    feat(nvim): Telescope search symbols

--- nvim/.config/nvim/lua/guimochila/plugins/telescope.lua
+++ nvim/.config/nvim/lua/guimochila/plugins/telescope.lua
@@ -4,8 +4,9 @@ return {
   -- or                              , branch = '0.1.x',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { '<leader>pf', ':Telescope find_files hidden=true<cr>', desc = 'Search Files' },
-    { '<C-p>',      ':Telescope git_files<cr>',              desc = 'Search git files' },
-    { '<leader>fg', ':Telescope live_grep<cr>',              desc = 'Grep search' },
+    { '<leader>pf', ':Telescope find_files hidden=true<cr>',        desc = 'Search Files' },
+    { '<C-p>',      ':Telescope git_files<cr>',                     desc = 'Search git files' },
+    { '<leader>fg', ':Telescope live_grep<cr>',                     desc = 'Grep search' },
+    { '<leader>fs', ':Telescope lsp_dynamic_workspace_symbols<cr>', desc = 'Search symbols' }
   }
 }
